0626d58f323f53da36eacf9b7513f7fb4a2bc60f
Haiyang Hong <hong961127@gmail.com>
2020-08-05 00:35:30 -0700

    Setup users and messages

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,36 @@
-const users = [{ id: 1, email: "123@gmail.com" }];
-let ID = 1;
+const users = [{ id: "1", email: "123@gmail.com", message: [] }];
+const messages = [
+  { id: "1", body: "what", createdAt: "2020-08-05T07:12:23.542Z", user: "1" },
+];
+let USER_ID = 1;
+let MSG_ID = 1;
 
 module.exports = {
   users: () => users,
-  createUser: (args) => {
-    ID++;
-    const newUser = { email: args.email, name: args.name, id: ID };
+  user: ({ id }) => {
+    console.log(id);
+    return users.find((user) => user.id === id);
+  },
+  messages: () =>
+    messages.map((message) => ({
+      ...message,
+      user: users.find((u) => u.id === message.user),
+    })),
+  createUser: ({ email, name }) => {
+    USER_ID++;
+    const newUser = { email: email, name: name, id: `${USER_ID}` };
     users.push(newUser);
     return newUser;
+  },
+  createMessage: ({ messageInput: { body, userId } }) => {
+    MSG_ID++;
+    const newMessage = {
+      id: `${MSG_ID}`,
+      body,
+      user: `${userId}`,
+      createdAt: new Date().toISOString(),
+    };
+    messages.push(newMessage);
+    return { ...newMessage, user: users.find((u) => u.id === newMessage.user) };
   },
 };
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -6,14 +6,30 @@ module.exports = buildSchema(`
     email: String!
     name: String
     avatarUrl: String
+    messages:[Message!]!
+  }
+
+  type Message {
+    id: ID!
+    body: String!
+    createdAt: String!
+    user: User!
+  }
+
+  input MessageInput {
+    body: String!
+    userId: ID!
   }
 
   type rootQuery {
     users: [User!]!
+    user(id:ID!):User
+    messages: [Message!]!
   }
 
   type rootMutation {
     createUser(email:String!,name:String!):User
+    createMessage(messageInput:MessageInput):Message
   }
 
   schema {
